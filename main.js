655cca91381c5b8dba1aeb774e28d821c791154e
LangeM36 <156849283+LangeM36@users.noreply.github.com>
2024-03-24 17:07:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+console.log("Matthew Lange");
+console.log("1999");
+console.log(1999);
+console.log("Matthew Lange (1999)");
+console.log("Matthew", "Lange", "(1999)");
+console.log("Matthew", "Lange", "(1999)");
+console.log("Matthew" + " Lange" + "(1999)");
+console.log("Matthew " + " Lange " + "(1999)");
